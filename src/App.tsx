c25de8f70f15c2d8b0932f9982b0cb4da0c4fe59
Manuilovalex <manuylovaleks@icloud.com>
2024-04-12 16:34:05 +0300

    React_test_library

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
-import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import UserProfile from './components/UserProfile';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <UserProfile />
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
